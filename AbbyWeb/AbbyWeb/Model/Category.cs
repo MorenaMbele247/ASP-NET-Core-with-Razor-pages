5b7f8795949c4267d355af5c50f132037ff79387
Morena Mbele <68221754+MorenaMbele247@users.noreply.github.com>
2022-10-13 23:31:23 +0200

    db connection

---
+++ AbbyWeb/AbbyWeb/Model/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }//enitiy framwork will automatically make this a primary key
+        [Required]
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+       
+    }
+}
